9e47b70411a6bc4b87a83f67b89047f3513246da
WalletMona.com <walletmona@gmail.com>
2014-02-09 23:25:53 +0900

    Refactor: extract wallet operations

--- send_coins.py
+++ send_coins.py
@@ -3,9 +3,8 @@
 from optparse import OptionParser
 import json
 
-from jsonrpc import ServiceProxy
-
 from config import Config
+from wallet import Wallet
 
 ##
 ## Process arguments
@@ -37,83 +36,20 @@ inputs = json.loads(args[1])
 address = args[2]
 amount = int(float(args[3]) * 10**8)
 
-config = Config(kind)
-
 
 ##
 ## Main
 ##
 
-## Deprecate after official supports for getrawchangeaddress in major coins
-def get_change_address(proxy):
-    groups = proxy.listaddressgroupings()
-    for group in groups:
-        for entry in group:
-            if len(entry) == 3:
-                ## maybe with account
-                continue
-            elif len(entry) == 2:
-                res = proxy.validateaddress(entry[0])
-                if 'account' not in res:
-                    return entry[0]
-            else:
-                raise RuntimeError('never reach')
-    return proxy.getnewaddress()
+config = Config(kind)
 
-
-proxy = ServiceProxy('http://%s:%s@%s:%d/' % (config.RPC_USER, config.RPC_PASS,
-                                              config.RPC_HOST, config.RPC_PORT))
-
-chgaddress = get_change_address(proxy)
-
-tmp = {address: float(amount) / 10**8, chgaddress: 1e-8}
-raw = proxy.createrawtransaction(inputs, tmp)
-size = len(raw) / 2
-
-## ref: wallet.cpp CWallet::CreateTransaction
-total = 0
-prio = 0
-for inp in inputs:
-    raw = proxy.getrawtransaction(inp['txid'])
-    tx = proxy.decoderawtransaction(raw)
-    value = tx['vout'][inp['vout']]['value']
-    conf = proxy.gettransaction(inp['txid'])['confirmations']
-
-    total += int(value * 10**8)
-    prio += int(value * 10**8) * conf
-prio = int(prio / size)
-
-payfee = config.PAY_TX_FEE * (1 + int(size / 1000))
-
-minfee = config.MIN_TX_FEE * (1 + int(size / 1000))
-if prio >= config.PRIO_THRESHOLD and size < config.FREE_TX_SIZE:
-    minfee = 0
-if amount < config.OUTPUT_THRESHOLD:
-    minfee += config.MIN_TX_FEE
-if total-amount-minfee < config.OUTPUT_THRESHOLD:
-    minfee += config.MIN_TX_FEE
-
-fee = max(payfee, minfee)
-
-change = total - amount - fee
-
-if change <= 0:
-    raise RuntimeError('Insufficient inputs: change = %f' %
-                        (float(change) / 10**8))
+wallet = Wallet(config.PAY_TX_FEE, config.MIN_TX_FEE, config.PRIO_THRESHOLD,
+                config.OUTPUT_THRESHOLD, config.FREE_TX_SIZE)
+wallet.connect(config.RPC_USER, config.RPC_PASS,
+               config.RPC_HOST, config.RPC_PORT)
 
 if opts.dryrun:
-    print('Total amount: %f' % (float(total) / 10**8))
-    print('Send: %f to %s' % (float(amount) / 10**8, address))
-    print('Change: %f to %s' % (float(change) / 10**8, chgaddress))
-    print('Fee: %f' % (float(fee) / 10**8))
-    print('Size: %d bytes' % size)
-    print('Priority: %d' % prio)
+    wallet.show_send_info(inputs, address, amount)
     print('Add --no-dry-run option to proceed')
 else:
-    out = {address: float(amount) / 10**8, chgaddress: float(change) / 10**8}
-    raw = proxy.createrawtransaction(inputs, out)
-    signed = proxy.signrawtransaction(raw)
-    if not signed['complete']:
-        raise RuntimeError('signatures are missing')
-    txid = proxy.sendrawtransaction(signed['hex'])
-    print(txid)
+    print(wallet.send(inputs, address, amount))
