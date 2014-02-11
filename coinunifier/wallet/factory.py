a63f32bb060c2ff49b4ac3091990e706f2faca48
WalletMona.com <walletmona@gmail.com>
2014-02-11 21:35:46 +0900

    Add support for Litecoin

--- coinunifier/wallet/factory.py
+++ coinunifier/wallet/factory.py
@@ -1,6 +1,8 @@
 
+from coinunifier.wallet.litecoin import LitecoinWallet
 from coinunifier.wallet.monacoin import MonacoinWallet
 
 def load_wallet(kind):
+    if kind == 'litecoin': return LitecoinWallet()
     if kind == 'monacoin': return MonacoinWallet()
     raise ArgumentError('No support for %s wallet' % (kind))
---
+++ coinunifier/wallet/litecoin.py
@@ -0,0 +1,28 @@
+
+import os
+
+from coinunifier.wallet.base import WalletBase
+
+DEFAULT_PAY_TX_FEE = int(0 * 10**8)
+DEFAULT_MIN_TX_FEE = int(0.001 * 10**8)
+
+PRIO_THRESHOLD = int(10**8 * 576 / 250)
+DUST_SOFT_LIMIT = int(10**8 * 0.001)
+
+FREE_TX_SIZE = 1000
+
+INPUT_SIZE = 41
+OUTPUT_SIZE = 34
+BASE_SIZE = 10
+
+DEFAULT_CONFIG_PATH = os.path.expanduser('~/.litecoin/litecoin.conf')
+
+class LitecoinWallet(WalletBase):
+
+    def __init__(self):
+        WalletBase.__init__(self, DEFAULT_PAY_TX_FEE, DEFAULT_MIN_TX_FEE,
+                            PRIO_THRESHOLD, DUST_SOFT_LIMIT, FREE_TX_SIZE)
+
+        self.set_size(BASE_SIZE, INPUT_SIZE, OUTPUT_SIZE)
+
+        self.load_config(DEFAULT_CONFIG_PATH)
